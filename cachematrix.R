139c90fdf7a6113e4d03cdf3bc8f1b176fdd8d33
rodserling <boblo.analytics@gmail.com>
2015-02-17 02:48:00 -0600

    Completed working version of cachematrix.R with added comments.

--- cachematrix.R
+++ cachematrix.R
@@ -2,18 +2,21 @@
 ## These 2 functions cache square functions and their inverse: 
 ##
 ##
-## The 1st function makeCacheMatrx() simply caches a matrix, retrieves the cached matrix, 
+## The 1st function makeCacheMatrx() creates a list of 4 functions which
+## simply caches a matrix, retrieves the cached matrix, 
 ## computes/caches the inverse of the matrix or retrieves the cached inverse.
+## More is discussed later about this function.
 ##
 ## The 2nd function cacheSolve() will only calculate the inverse of a matrix 
 ## if it isn't yet cached.  Either way the inverse is returned.
+## More is discussed later about this function.
 ##
 ## Specifically makeCacheMatrix(mat1=matrix()) will cache 'mat1' ( e.g.mat2<-makeCacheMatrix(mat1) )
 ## and return a list with the following functions:
 ##  -cache a matrix mat1 ( mat2$set(mat1)), 
 ##  -return the cached matrix, if it exists ( mat2$get() ) 
 ##  -find the inverse of a matrix and cache it ( mat2$setSolve(solve(mat1) )
-##  -return the cached matrix inverse ( mat2$getSolve() )
+##  -return the cached matrix inverse if it exists ( mat2$getSolve() )
 ##
 makeCacheMatrix <- function(x = matrix()) {
       m<- NULL
@@ -29,8 +32,8 @@ makeCacheMatrix <- function(x = matrix()) {
            getSolve = getSolve)
 }
 ##
-## cacheSolve(mat2=list()) shall first check if the inverse of the matrix is already cached.
-## If so, the user is prompted that the inverse exists and the inverse matrix is 
+## cacheSolve(mat2=list()) shall first check if the inverse of the matrix is already cached
+## in the list.  If so, the user is prompted that the inverse exists and the inverse matrix is 
 ## returned from the cache.  If the inverse doesn't exist, the inverse is calculated, 
 ## cached and returned to the user.
 ##
